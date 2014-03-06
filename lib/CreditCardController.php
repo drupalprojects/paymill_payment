d101a2d0a0822bcf013197036abf2facee473c41
phaer <phaer@phaer.org>
2014-03-06 22:40:05 +0100

    add api calls for one-time credit card payments, they are working now!

--- lib/CreditCardController.php
+++ lib/CreditCardController.php
@@ -11,5 +11,19 @@ class CreditCardController extends \Drupal\paymill_payment\CommonController {
     $this->title = t('Paymill Credit Card');
     $this->form = new \Drupal\paymill_payment\PaymillCreditCardForm();
   }
+
+  public function execute(\Payment $payment) {
+    $api_key = $payment->method->controller_data['private_key'];
+    $token = $payment->method_data['paymill_payment_token'];
+    $request = new \Paymill\Request($api_key);
+    $transaction = new \Paymill\Models\Request\Transaction();
+
+    $transaction
+      ->setToken($token)
+      ->setAmount($payment->totalAmount(0))
+      ->setCurrency($payment->currency_code)
+      ->setDescription(t('Donation to ' . variable_get('site_name',
+            t('an unnamed site'))));
+    $response = $request->create($transaction);
   }
 }
