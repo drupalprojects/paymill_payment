43f4af9e8626b28c84098f5b600c9079991e99b4
phaer <phaer@phaer.org>
2014-03-18 20:08:54 +0100

    add all supported creditcard issuers.

--- lib/PaymillCreditCardForm.php
+++ lib/PaymillCreditCardForm.php
@@ -10,6 +10,28 @@ namespace Drupal\paymill_payment;
 
 
 class PaymillCreditCardForm extends \Drupal\payment_forms\CreditCardForm {
+  static protected $issuers = array(
+    'visa'           => 'Visa',
+    'mastercard'     => 'MasterCard',
+    'amex'           => 'American Express',
+    'jcb'            => 'JCB',
+    'maestro_uk'     => 'Maestro UK',
+    'carte_bleue'    => 'Carte Bleue',
+    'diners_club'    => 'Diners Club',
+    'discover'       => 'Discover',
+    'china_unionpay' => 'China UnionPay',
+  );
+  static protected $cvc_label = array(
+    'visa'           => 'CVV2 (Card Verification Value 2)',
+    'amex'           => 'CID (Card Identification Number)',
+    'mastercard'     => 'CVC2 (Card Validation Code 2)',
+    'jcb'            => 'CSC (Card Security Code)',
+    'maestro_uk'     => 'CSC (Card Security Code)',
+    'carte_bleue'    => 'CSC (Card Security Code)',
+    'diners_club'    => 'CSC (Card Security Code)',
+    'discover'       => 'CID (Card Identification Number)',
+    'china_unionpay' => 'CSC (Card Security Code)',
+  );
 
   public function getForm(array &$form, array &$form_state) {
     parent::getForm($form, $form_state);
